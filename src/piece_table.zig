439541791b3495b4bfbe7032279af0bb1c6d4fcb
Brian Jones <bojo@bojo.wtf>
2024-10-08 11:10:36 -0800

    much better management of all things

--- src/piece_table.zig
+++ src/piece_table.zig
@@ -1,13 +1,14 @@
 const std = @import("std");
 const testing = std.testing;
-const SinglyLinkedList = std.SinglyLinkedList;
+const Allocator = std.mem.Allocator;
+const ArrayList = std.ArrayList;
 
 // Each subsequent command builds off the representation
 // of the culmination of everything prior. In a way this
 // is more like append-only Event Sourcing where the result
 // is the sum of all prior commands.
 //
-// Piece { .op, .string, .start, .length }
+// Piece { .op, .string, .start }
 //
 // Op.origin + Op.add (=new origin) + Op.delete (=new origin):
 //
@@ -40,94 +41,108 @@ pub fn PieceTable() type {
     return struct {
         const Self = @This();
 
-        root: SinglyLinkedList(Piece) = SinglyLinkedList(Piece){},
+        allocator: std.mem.Allocator,
+        root: ArrayList(Piece),
 
         const Piece = struct {
             op: Op,
             string: []const u8,
             start: usize,
-            length: usize,
+
+            fn init(op: Op, string: []const u8, start: usize) Piece {
+                return Piece{
+                    .op = op,
+                    .string = string,
+                    .start = start,
+                };
+            }
         };
 
-        pub fn init(self: *Self, alloc: std.mem.Allocator, string: []const u8) !void {
-            try self.append(alloc, Op.origin, string, 0);
+        pub fn init(allocator: Allocator) Self {
+            const root = ArrayList(Piece).init(allocator);
+            return .{
+                .allocator = allocator,
+                .root = root,
+            };
         }
 
         pub fn len(self: *Self) usize {
-            return self.root.len();
+            return self.root.items.len;
         }
 
-        pub fn append(self: *Self, alloc: std.mem.Allocator, op: Op, string: []const u8, s: usize) !void {
-            const Node = std.SinglyLinkedList(Piece).Node;
-            var node_ptr = try alloc.create(Node);
-            node_ptr.data = Piece{
-                .op = op,
-                .string = string,
-                .start = s,
-                .length = string.len,
-            };
-            self.root.prepend(node_ptr);
+        pub fn append(self: *Self, op: Op, string: []const u8, start: usize) !void {
+            const piece = Piece.init(op, string, start);
+            try self.root.append(piece);
         }
 
-        pub fn replay(self: *Self, alloc: std.mem.Allocator) ![]const u8 {
-            var origin = std.ArrayList(u8).init(alloc);
+        // Caller owns and must free the returned slice with the same allocator
+        pub fn replay(self: *Self) ![]const u8 {
+            var origin = std.ArrayList(u8).init(self.allocator);
             defer origin.deinit();
 
-            std.SinglyLinkedList(Piece).Node.reverse(&self.root.first);
-            var it = self.root.first;
-            while (it) |node| {
-                switch (node.data.op) {
-                    Op.origin => try origin.appendSlice(node.data.string),
-                    Op.add => try origin.insertSlice(node.data.start, node.data.string),
-                    Op.delete => {
-                        for (0..node.data.length) |_| {
-                            _ = origin.orderedRemove(node.data.start);
-                        }
-                    },
+            if (self.root.items.len > 0) {
+                var i: usize = 0;
+                while (i < self.root.items.len) : (i += 1) {
+                    const piece = self.root.items[i];
+                    switch (piece.op) {
+                        Op.origin => try origin.appendSlice(piece.string),
+                        Op.add => try origin.insertSlice(piece.start, piece.string),
+                        Op.delete => {
+                            for (0..piece.string.len) |_| {
+                                _ = origin.orderedRemove(piece.start);
+                            }
+                        },
+                    }
                 }
-                it = it.?.next;
             }
-            std.SinglyLinkedList(Piece).Node.reverse(&self.root.first);
+
             return origin.toOwnedSlice();
         }
 
-        pub fn deinit(self: *Self, alloc: std.mem.Allocator) void {
-            var it = self.root.first;
-            while (it) |node| : (it = it.?.next) {
-                alloc.destroy(node);
-            }
+        pub fn deinit(self: *Self) void {
+            self.root.deinit();
         }
     };
 }
 
-const PieceTableTest = PieceTable();
+test "Piece: init" {
+    const piece = PieceTable().Piece.init(Op.origin, "Hello World\n", 0);
+    try testing.expect(piece.op == Op.origin);
+    try testing.expect(std.mem.eql(u8, "Hello World\n", piece.string));
+    try testing.expect(piece.start == 0);
+}
 
 test "PieceTable: init, append, len, replay" {
-    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
-    defer arena.deinit();
-    const alloc = arena.allocator();
+    const allocator = testing.allocator;
 
-    var pt = PieceTableTest{};
-    defer pt.deinit(alloc);
+    var pt = PieceTable().init(allocator);
+    defer pt.deinit();
 
-    try pt.init(alloc, "Hello World\n");
-    try testing.expectEqual(pt.root.first.?.data.string, "Hello World\n");
+    try testing.expect(pt.len() == 0);
+    try pt.append(Op.origin, "Hello World\n", 0);
     try testing.expect(pt.len() == 1);
+    try testing.expectEqual(pt.root.items[0].string, "Hello World\n");
+    var result = try pt.replay();
+    try testing.expect(std.mem.eql(u8, "Hello World\n", result));
+    allocator.free(result);
 
-    try pt.append(alloc, Op.add, "There ", 6);
+    try pt.append(Op.add, "There ", 6);
     try testing.expect(pt.len() == 2);
-    var result = try pt.replay(alloc);
+    result = try pt.replay();
     try testing.expect(std.mem.eql(u8, "Hello There World\n", result));
+    allocator.free(result);
 
-    try pt.append(alloc, Op.delete, "llo The", 2);
+    try pt.append(Op.delete, "llo The", 2);
     try testing.expect(pt.len() == 3);
-    result = try pt.replay(alloc);
+    result = try pt.replay();
     try testing.expect(std.mem.eql(u8, "Here World\n", result));
+    allocator.free(result);
 
-    try pt.append(alloc, Op.delete, "Here", 0);
-    try pt.append(alloc, Op.add, "Sup", 0);
+    try pt.append(Op.delete, "Here", 0);
+    try pt.append(Op.add, "Sup", 0);
     try testing.expect(pt.len() == 5);
 
-    result = try pt.replay(alloc);
+    result = try pt.replay();
     try testing.expect(std.mem.eql(u8, "Sup World\n", result));
+    allocator.free(result);
 }
